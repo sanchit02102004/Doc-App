f70b8b3b554fc2bc7da487f0991b2506e5104761
sanchit <sanchit.20223229@mnnit.ac.in>
2024-03-16 14:53:42 +0530

    first commit

---
+++ video-chat-app/src/App.jsx
@@ -0,0 +1,76 @@
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
+import "./App.css";
+import JoinForm from "./Components/JoinForm";
+import { useEffect } from "react";
+import Conference from "./Components/Conference";
+import Footer from "./Components/Footer";
+
+function App() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+
+  useEffect(() => {
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave();
+      }
+    };
+  }, [hmsActions, isConnected]);
+
+  return (
+    <div className="App">
+      {isConnected ? (
+        <>
+          <Conference />
+          <Footer />
+        </>
+      ) : (
+        <JoinForm />
+      )}
+    </div>
+  );
+}
+
+export default App;
+// import {
+//   selectIsConnectedToRoom,
+//   useHMSActions,
+//   useHMSStore,
+// } from "@100mslive/react-sdk";
+// import "./App.css";
+// import { useEffect } from "react";
+// import Conference from "./Components/Conference";
+// import Footer from "./Components/Footer";
+// import JoinForm from "./Components/JoinForm";
+
+// function App() {
+//   const isConnected = useHMSStore(selectIsConnectedToRoom);
+//   const hmsActions = useHMSActions();
+
+//   useEffect(() => {
+//     window.onunload = () => {
+//       if (isConnected) {
+//         hmsActions.leave();
+//       }
+//     };
+//   }, [hmsActions, isConnected]);
+
+//   return (
+//     <div className="App">
+//       {isConnected ? (
+//         <>
+//           <Conference />
+//           <Footer />
+//         </>
+//       ) : (
+//         <JoinForm />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
